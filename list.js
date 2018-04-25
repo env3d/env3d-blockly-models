904582ada8b67e1083497b15b40a3eb77d40a279
Jason Madar <jason@vandrico.com>
2018-04-24 19:37:46 -0700

    added some fbx test houses

--- list.js
+++ list.js
@@ -1,32 +1,40 @@
 [
     {
+        "mtl": "dino/obj.mtl",
         "obj": "dino/tinker.obj",
-        "name": "dino",
-        "mtl": "dino/obj.mtl"
+        "name": "dino"
     },
     {
+        "mtl": "gas-station/gasstation.mtl",
         "obj": "gas-station/GasStation.obj",
-        "name": "gas-station",
-        "mtl": "gas-station/gasstation.mtl"
+        "name": "gas-station"
     },
     {
+        "mtl": "house/obj.mtl",
         "obj": "house/tinker.obj",
-        "name": "house",
-        "mtl": "house/obj.mtl"
+        "name": "house"
     },
     {
         "obj": "intersection/intersection.obj",
-        "name": "intersection",
-        "mtl": "intersection/materials.mtl"
+        "mtl": "intersection/materials.mtl",
+        "name": "intersection"
     },
     {
+        "mtl": "rio/materials.mtl",
         "obj": "rio/model-triangulated.obj",
-        "name": "rio",
-        "mtl": "rio/materials.mtl"
+        "name": "rio"
     },
     {
+        "fbx": "test-house-a/testHouse_A.fbx",
+        "name": "test-house-a"
+    },
+    {
+        "fbx": "test-house-b/testHouse_B.fbx",
+        "name": "test-house-b"
+    },
+    {
+        "mtl": "titanic/materials.mtl",
         "obj": "titanic/model-triangulated.obj",
-        "name": "titanic",
-        "mtl": "titanic/materials.mtl"
+        "name": "titanic"
     }
 ]
