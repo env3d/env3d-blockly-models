10690a7a1afa96002e91071ee1cb9465ff14badb
Jason Madar <jason@vandrico.com>
2018-06-06 16:00:33 -0700

    genereated all 200+ models into list

--- list.py
+++ list.py
@@ -1,5 +1,6 @@
 import os
 import json
+import itertools
 from functools import reduce
 
 def firstElem(ary):
@@ -11,9 +12,7 @@ def reducer(x, y):
     x[ext] = y
     return x
 
-# given a directory, find the relevant obj, mtl, and image files
-def process_dir(d):
-    name = d
+def process_single_model(d):
     files = [d+'/'+f for f in os.listdir(d) if os.path.isfile(d+'/'+f) and not f.startswith('.')]
     # add each file type into the same object
     obj = reduce(reducer, [{}]+files)
@@ -23,9 +22,25 @@ def process_dir(d):
     obj['name'] = d
     return obj
 
+# d is an assets directory, with a list of json
+def process_assets(d):
+    json_files = [ { 'fbx': d+'/assets/'+f, 'name': d+'/'+f.split('.')[0] }
+                   for f in os.listdir(d+'/assets') if f.endswith('json') ]
+    return json_files
+
+    
+# given a directory, find the relevant obj, mtl, and image files
+def process_dir(d):
+    name = d
+    if (os.path.isdir(d+'/assets')):
+        return process_assets(d)
+    else:
+        return [process_single_model(d)]
+
 dirs = os.listdir('./')
 l = [ process_dir(d) for d in dirs if os.path.isdir(d) and not d.startswith('.')]
 
+ll = list(itertools.chain(*l));
 
-print(json.dumps(l, indent=4,separators=(',', ': ')))
+print(json.dumps(ll, indent=4,separators=(',', ': ')))
 
